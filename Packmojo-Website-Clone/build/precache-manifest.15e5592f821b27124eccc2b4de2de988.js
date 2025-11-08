ab665cd9eba7a9a0374a17e75a49c7c8e4fbd246
Project Wellverse <projectwellverse24@outlook.com@savannahinc.dckh22tqt0eelfbky2v2lhzajh.bx.internal.cloudapp.net>
2025-11-08 17:02:40 +0000

    Apply stashed changes except settings.py

---
+++ Packmojo-Website-Clone/build/precache-manifest.15e5592f821b27124eccc2b4de2de988.js
@@ -0,0 +1,374 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7943eb6706d5b7640f398a7386aaffe9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c7de79eaa6ee7c91fa4",
+    "url": "/static/css/2.7f5860a0.chunk.css"
+  },
+  {
+    "revision": "b2ad861dacd0081919ab",
+    "url": "/static/css/main.aff434b3.chunk.css"
+  },
+  {
+    "revision": "0c7de79eaa6ee7c91fa4",
+    "url": "/static/js/2.86811f39.chunk.js"
+  },
+  {
+    "revision": "ce03391d82aa81ea39038e6d56e0cb31",
+    "url": "/static/js/2.86811f39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ad861dacd0081919ab",
+    "url": "/static/js/main.886dc9fb.chunk.js"
+  },
+  {
+    "revision": "7aa7b308304f1ff25ffc",
+    "url": "/static/js/runtime-main.b9a8bbb4.js"
+  },
+  {
+    "revision": "ff9f13316d81adc28e49580cd3984292",
+    "url": "/static/media/6.ff9f1331.png"
+  },
+  {
+    "revision": "516c28aedba4a6a827f21459f8b6d5ad",
+    "url": "/static/media/7.516c28ae.png"
+  },
+  {
+    "revision": "455244429d80f69cd422624353b4746d",
+    "url": "/static/media/Azure.45524442.jpg"
+  },
+  {
+    "revision": "1fb1a2f2a21e2d13811472963cffaa14",
+    "url": "/static/media/CRMICON.1fb1a2f2.svg"
+  },
+  {
+    "revision": "d4ad03f436d4ed2e80f0837c0d4ae0c2",
+    "url": "/static/media/Dell-XPS-15-7590-Laptop.d4ad03f4.webp"
+  },
+  {
+    "revision": "f3729150cebcaed6aada148338ab3ea2",
+    "url": "/static/media/ERP3.f3729150.jpg"
+  },
+  {
+    "revision": "4b2e3c04abed916a8ca5d6e5607cf0bc",
+    "url": "/static/media/ERPICON.4b2e3c04.svg"
+  },
+  {
+    "revision": "57bb70b21b587e2f9dfce80fc69a7277",
+    "url": "/static/media/HP-34F-Monitor.57bb70b2.jpg"
+  },
+  {
+    "revision": "fb972afacc9b09a07e0a67162b5b2617",
+    "url": "/static/media/HP-DeskJet-2320.fb972afa.jpg"
+  },
+  {
+    "revision": "4112315d3c45d1318215951034b474a0",
+    "url": "/static/media/POS1.4112315d.png"
+  },
+  {
+    "revision": "abbfbd98a7281e4b0e319847db76234e",
+    "url": "/static/media/PesaPal2.abbfbd98.jpeg"
+  },
+  {
+    "revision": "8cdcb9413ccf288dc125ec19ff6392b5",
+    "url": "/static/media/Salesforce.8cdcb941.jpeg"
+  },
+  {
+    "revision": "1a3960d91a50673afed1a8d3e800deab",
+    "url": "/static/media/_BusinessCentral2.1a3960d9.png"
+  },
+  {
+    "revision": "4b2e3c04abed916a8ca5d6e5607cf0bc",
+    "url": "/static/media/accounting&finance.4b2e3c04.svg"
+  },
+  {
+    "revision": "f63ca379b0df51c96c7322081e36ad4f",
+    "url": "/static/media/activity-managemencrm.f63ca379.svg"
+  },
+  {
+    "revision": "a8746db6abca66e6faf9a351b1fb09c0",
+    "url": "/static/media/annot.a8746db6.jpeg"
+  },
+  {
+    "revision": "888cc389995053863aff7cbdbff25ecc",
+    "url": "/static/media/bac1.888cc389.jpg"
+  },
+  {
+    "revision": "d780d57320b6ebd6222c493b6ad992f5",
+    "url": "/static/media/bac2.d780d573.jpg"
+  },
+  {
+    "revision": "1ecadd1b2137808f2dfb1ee480de512d",
+    "url": "/static/media/bac3.1ecadd1b.jpg"
+  },
+  {
+    "revision": "7a17f5e30547b1530265cbafb32eeab2",
+    "url": "/static/media/bac4.7a17f5e3.jpg"
+  },
+  {
+    "revision": "6924360773c7f2f9f3f459ae43d648f7",
+    "url": "/static/media/bac7.69243607.jpg"
+  },
+  {
+    "revision": "ed20abf60e6fc4c75c75cc492229caac",
+    "url": "/static/media/bankingerp.ed20abf6.svg"
+  },
+  {
+    "revision": "3cc1460916c0ec8ff2275a7da8675ebc",
+    "url": "/static/media/business.3cc14609.jpg"
+  },
+  {
+    "revision": "7bbf6acb5c2d964b268d7e71d390ebd9",
+    "url": "/static/media/businessICON.7bbf6acb.svg"
+  },
+  {
+    "revision": "c22bd4b6164f498f8a550ed6d27f8ece",
+    "url": "/static/media/chatbot.c22bd4b6.jpeg"
+  },
+  {
+    "revision": "bfe439e2130e5faa040131e93446d84e",
+    "url": "/static/media/comp1.bfe439e2.png"
+  },
+  {
+    "revision": "ec8908c22ce8b8f7e9f60b374068505b",
+    "url": "/static/media/compliance.ec8908c2.svg"
+  },
+  {
+    "revision": "31fa679cfafed2187ff9e8d3f0f32342",
+    "url": "/static/media/crm2.31fa679c.jpg"
+  },
+  {
+    "revision": "9b664a7b97a112e02c2aa779e9dc73bb",
+    "url": "/static/media/crm3.9b664a7b.jpg"
+  },
+  {
+    "revision": "39433e84d1bcaa26541c90575bcd2f99",
+    "url": "/static/media/customer-managementcrm.39433e84.svg"
+  },
+  {
+    "revision": "4c93dd44a81543c0f2fd25c625aec2ed",
+    "url": "/static/media/customer2.4c93dd44.png"
+  },
+  {
+    "revision": "06ce8af0935c2cb898e4c5373937ff6b",
+    "url": "/static/media/data1.06ce8af0.png"
+  },
+  {
+    "revision": "4251a9be5d20888dce91d0ca91d9fbf1",
+    "url": "/static/media/demographic-analysismarketing.4251a9be.svg"
+  },
+  {
+    "revision": "decddf4289b6269f04173c6de4a4a488",
+    "url": "/static/media/desktop.decddf42.webp"
+  },
+  {
+    "revision": "540ece2259e386579537151c4dd6ae88",
+    "url": "/static/media/dynamics customers.540ece22.png"
+  },
+  {
+    "revision": "0430e17aa8a0c062e557ae927c3e90b6",
+    "url": "/static/media/erp2.0430e17a.png"
+  },
+  {
+    "revision": "b3fd98cd9c00012d7a31895e41a60af7",
+    "url": "/static/media/future.b3fd98cd.png"
+  },
+  {
+    "revision": "d144ae960f2b6159cab23c689947b2bf",
+    "url": "/static/media/future2.d144ae96.png"
+  },
+  {
+    "revision": "19cdaca418bbf785ecacfa696da4ee9f",
+    "url": "/static/media/future3.19cdaca4.png"
+  },
+  {
+    "revision": "6a0e667147e9f35b995d4d582c182427",
+    "url": "/static/media/gold.6a0e6671.jpeg"
+  },
+  {
+    "revision": "3c2b40a75c0d3043b55fc90121c920d1",
+    "url": "/static/media/harddrive.3c2b40a7.jpg"
+  },
+  {
+    "revision": "a0104f9413886d6a6cc89b57f280661f",
+    "url": "/static/media/hrms.a0104f94.svg"
+  },
+  {
+    "revision": "de6b5cbb44029dadb7be72baad17593f",
+    "url": "/static/media/ims.de6b5cbb.jpeg"
+  },
+  {
+    "revision": "9218d0e1e045ec58d87c6f447f16e166",
+    "url": "/static/media/ims4.9218d0e1.svg"
+  },
+  {
+    "revision": "d773b612633b56a914604bc6550ff1da",
+    "url": "/static/media/interactive.d773b612.svg"
+  },
+  {
+    "revision": "3b7a5726ed040286c62d04c7e058f889",
+    "url": "/static/media/inventory-analyticsims.3b7a5726.svg"
+  },
+  {
+    "revision": "2ed7eef15a16739aa630cc22d0942997",
+    "url": "/static/media/inventoryims.2ed7eef1.svg"
+  },
+  {
+    "revision": "2011a8ea4fa20499d1ec29d6e4c5900e",
+    "url": "/static/media/lead-managementcrm.2011a8ea.svg"
+  },
+  {
+    "revision": "539cfe72a6a329ea6825e4384b2c841f",
+    "url": "/static/media/lenovox1carbon.539cfe72.jpg"
+  },
+  {
+    "revision": "1eec3f342adacac821a7bf46e0a595ac",
+    "url": "/static/media/line.1eec3f34.svg"
+  },
+  {
+    "revision": "1a72472b25cd90178383cf1e3a6c2845",
+    "url": "/static/media/lms.1a72472b.png"
+  },
+  {
+    "revision": "08a9511da4382f9379a6db2e6bd88d9a",
+    "url": "/static/media/logisticsICON.08a9511d.svg"
+  },
+  {
+    "revision": "e84d3696529d6fb628a059dbe66ac127",
+    "url": "/static/media/logo4.e84d3696.jpg"
+  },
+  {
+    "revision": "4a6446bc33d8c3d7a2e6e5a384821ba1",
+    "url": "/static/media/macbook2019.4a6446bc.jpg"
+  },
+  {
+    "revision": "4cde29505950085c7acdfb3d9f06d0a4",
+    "url": "/static/media/mechanical keyboard.4cde2950.jpeg"
+  },
+  {
+    "revision": "796355eb2ae6130fee3bf18a1a31d0ef",
+    "url": "/static/media/mouse4.796355eb.webp"
+  },
+  {
+    "revision": "bba35c7dc8a5845f3e6801f9f91e6e8f",
+    "url": "/static/media/nairobi2.bba35c7d.jpg"
+  },
+  {
+    "revision": "1757c0d3f48848f313f6bd681801ff80",
+    "url": "/static/media/one-hub.1757c0d3.svg"
+  },
+  {
+    "revision": "c02057b99411d7ac1850f3d4d75f8640",
+    "url": "/static/media/performance-analysismarketing.c02057b9.svg"
+  },
+  {
+    "revision": "7f129ccdf34dbe9344f95f5428f58fcb",
+    "url": "/static/media/pms1.7f129ccd.png"
+  },
+  {
+    "revision": "b2761769ca111a0dc24d566953b294a9",
+    "url": "/static/media/pms2.b2761769.png"
+  },
+  {
+    "revision": "8f088205bf4e6e982abdc75f9782d68c",
+    "url": "/static/media/pos2.8f088205.png"
+  },
+  {
+    "revision": "9fe29be742539a7b1434eebe9b8621db",
+    "url": "/static/media/procurement-managementscm.9fe29be7.svg"
+  },
+  {
+    "revision": "0bca06964eea29fd2396b8cc5238b1c1",
+    "url": "/static/media/project operations.0bca0696.png"
+  },
+  {
+    "revision": "3a9c259302c59c617bb924b381190df4",
+    "url": "/static/media/quality-controlscm.3a9c2593.svg"
+  },
+  {
+    "revision": "5d472eb6651f25d446429bd88552b828",
+    "url": "/static/media/reorder-ims.5d472eb6.svg"
+  },
+  {
+    "revision": "138ccf40fe52e70fcc8145c3f4c08784",
+    "url": "/static/media/reports-crm.138ccf40.svg"
+  },
+  {
+    "revision": "056e4aa197c1939a661f48386884b940",
+    "url": "/static/media/saleserp.056e4aa1.svg"
+  },
+  {
+    "revision": "7ddc1376de469e79dd043a94f2a268a2",
+    "url": "/static/media/scm1.7ddc1376.jpg"
+  },
+  {
+    "revision": "61791f46d3b3a5231445e83f5a6a035a",
+    "url": "/static/media/scm2.61791f46.png"
+  },
+  {
+    "revision": "f43e10f07a3937f9d329c2898d5c2416",
+    "url": "/static/media/servercore.f43e10f0.jpg"
+  },
+  {
+    "revision": "032a74ad9dfe2405c75650ae08ff78b1",
+    "url": "/static/media/shipping-logisticsscm.032a74ad.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "efae9401dede6aa57d40c17230cc6f80",
+    "url": "/static/media/supplier-managementscm.efae9401.svg"
+  },
+  {
+    "revision": "efc8f32a9830ab41925efa6dbbd2a343",
+    "url": "/static/media/supply-chain-analyticsscm.efc8f32a.svg"
+  },
+  {
+    "revision": "f6812cca635a00faa6757b8de29e7c09",
+    "url": "/static/media/support1.f6812cca.png"
+  },
+  {
+    "revision": "68deb15c5c52904c04a353bc62183e04",
+    "url": "/static/media/tableau1.68deb15c.jpg"
+  },
+  {
+    "revision": "751d47ef56d0803beb28243651b407b0",
+    "url": "/static/media/thermal1.751d47ef.jpg"
+  },
+  {
+    "revision": "be45878e7e8ce649b585e6d30f890a98",
+    "url": "/static/media/training.be45878e.png"
+  },
+  {
+    "revision": "bee179b1af86da050b1d53e1ee4b2825",
+    "url": "/static/media/website.bee179b1.png"
+  },
+  {
+    "revision": "0e235bf613b62e704875c8fe6065f9a2",
+    "url": "/static/media/whatsapp.0e235bf6.png"
+  },
+  {
+    "revision": "a867118ee3c5e2679b7324f720e7633d",
+    "url": "/static/media/zoho.a867118e.png"
+  },
+  {
+    "revision": "a867118ee3c5e2679b7324f720e7633d",
+    "url": "/static/media/zoho1.a867118e.png"
+  }
+]);
